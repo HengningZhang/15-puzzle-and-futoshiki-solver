d65f83076012f970d8423e6d7e260e525c0842d5
HengningZhang <hz1704@nyu.edu>
2020-04-24 10:50:11 -0400

    functional version

--- Project2/main.py
+++ Project2/main.py
@@ -10,10 +10,10 @@ def read(filename):
         start.append(f.readline().split(" "))
         for j in range(5):
             start[i][j]=int(start[i][j])
-    garbage=f.readline()
+    f.readline()
     for i in range(5):
         leftRight.append(f.readline().split(" "))
-    garbage=f.readline()
+    f.readline()
     for i in range(4):
         upDown.append(f.readline().split(" "))
     return start,leftRight,upDown
@@ -27,18 +27,16 @@ class node:
         self.udc=upDownConstraints
         self.domains=generateDomains(board)
         self.valid=forwardChecking(self.domains,self.lrc,self.udc)
-        self.mcdv=mostConstrainedVariable(self.board,self.domains)
+        self.mcdv,self.constrainingNum=mostConstrainVariable(self.board,self.domains)
         self.lenMCDV=len(self.domains[self.mcdv[0]][self.mcdv[1]])
-        self.mcgv=mostConstrainingVariable(self.board)
-        self.lenMCGV=len(self.domains[self.mcgv[0]][self.mcgv[1]])
     def __lt__(self,other):
         if self.lenMCDV==other.lenMCDV:
-            return self.lenMCGV<other.lenMCGV
+            return self.constrainingNum>other.constrainingNum
         return self.lenMCDV<other.lenMCDV
     def __gt__(self,other):
         return other<self
     def __eq__(self,other):
-        return self.lenMCDV==other.lenMCDV and self.lenMCGV==other.lenMCGV
+        return self.lenMCDV==other.lenMCDV and self.constrainingNum==other.constrainingNum
 
 def checkFilled(board):
     for i in range(5):
@@ -65,36 +63,27 @@ def generateDomains(board):
 
 def forwardChecking(domains,leftRightConstraints,upDownConstraints):
     valid=True
-    for i in range(5):
-        for j in range(4):
-            if leftRightConstraints[i][j][0]==">":
-                for k in range(max(domains[i][j]),6):
-                    if k in domains[i][j+1]:
-                        domains[i][j+1].remove(k)
-            elif leftRightConstraints[i][j][0]=="<":
-                for k in range(max(domains[i][j+1]),6):
-                    if k in domains[i][j]:
-                        domains[i][j].remove(k)
-    for i in range(4):
-        for j in range(5):
-            if upDownConstraints[i][j][0]=="v":
-                for k in range(max(domains[i][j]),6):
-                    if k in domains[i+1][j]:
-                        domains[i+1][j].remove(k)
-            elif upDownConstraints[i][j][0]=="^":
-                for k in range(max(domains[i+1][j]),6):
-                    if k in domains[i][j]:
-                        domains[i][j].remove(k)
-    for i in range(5):
-        for j in range(4):
-            if leftRightConstraints[i][j][0]==">":
-                for k in range(max(domains[i][j]),6):
-                    if k in domains[i][j+1]:
-                        domains[i][j+1].remove(k)
-            elif leftRightConstraints[i][j][0]=="<":
-                for k in range(max(domains[i][j+1]),6):
-                    if k in domains[i][j]:
-                        domains[i][j].remove(k)
+    for n in range(5):
+        for i in range(5):
+            for j in range(4):
+                if leftRightConstraints[i][j][0]==">":
+                    for k in range(max(domains[i][j]),6):
+                        if k in domains[i][j+1]:
+                            domains[i][j+1].remove(k)
+                elif leftRightConstraints[i][j][0]=="<":
+                    for k in range(max(domains[i][j+1]),6):
+                        if k in domains[i][j]:
+                            domains[i][j].remove(k)
+        for i in range(4):
+            for j in range(5):
+                if upDownConstraints[i][j][0]=="v":
+                    for k in range(max(domains[i][j]),6):
+                        if k in domains[i+1][j]:
+                            domains[i+1][j].remove(k)
+                elif upDownConstraints[i][j][0]=="^":
+                    for k in range(max(domains[i+1][j]),6):
+                        if k in domains[i][j]:
+                            domains[i][j].remove(k)
     for i in range(5):
         for j in range(5):
             if not domains[i][j]:
@@ -104,38 +93,46 @@ def mostConstrainVariable(board,domains):
     minCount=5
     maxCount=0
     mcdv=(0,0)
-    mcgv=(0,0)
+    constrainingNum=0
     for i in range(5):
         for j in range(5):
-            if len(domains[i][j])<minCount and board[i][j]==0:
-                minCount=len(domains[i][j])
-                mcdv=(i,j)
+            if board[i][j]==0 and len(domains[i][j])<=minCount:
                 counter=board[i].count(0)-1
                 for k in range(5):
                     if board[k][j]==0:
                         counter+=1
                 counter-=1
-                if counter>maxCount:
-                    mcgv=(i,j)
-    return mcdv,mcgv
-# def mostConstrainingVariable(board):
-#     maxCount=0
-#     output=(0,0)
-#     for i in range(5):
-#         for j in range(5):
-#             if board[i][j]==0:
-#                 counter=board[i].count(0)
-#                 for k in range(5):
-#                     if board[k][j]==0:
-#                         counter+=1
-#                 if counter>maxCount:
-#                     output=(i,j)
-#     return output
+                if len(domains[i][j])<minCount:
+                    minCount=len(domains[i][j])
+                    mcdv=(i,j)
+                    constrainingNum=counter
+                    maxCount=counter
+                elif len(domains[i][j])==minCount:
+                    if counter>maxCount:
+                        mcdv=(i,j)
+                        constrainingNum=counter
+                        maxCount=counter
+    return mcdv,constrainingNum
 
-def backTrack(node):
-    if node.status==True:
-        return node.board
-    unassigned=node.mc
+
+def backTrack(anode):
+    frontier=[]
+    frontier.append(anode)
+    heapq.heapify(frontier)
+    while frontier:
+        current=heapq.heappop(frontier)
+        if current.status==True:
+            return current.board
+        index=current.mcdv
+        domain=current.domains[index[0]][index[1]]
+        for i in range(len(domain)):
+            newBoard=copy.deepcopy(current.board)
+            newBoard[index[0]][index[1]]=domain[i]
+            newNode=node(newBoard,current.lrc,current.udc)
+            if newNode.valid==True:
+                heapq.heappush(frontier,newNode)
+    return False
+
 
 data=read("input1.txt")
 for elem in generateDomains(data[0]):
@@ -148,6 +145,5 @@ anode=node(data[0],data[1],data[2])
 for elem in anode.domains:
     print(elem)
 
-print(anode.mcdv)
-print(anode.mcgv)
-print(anode.board)
+
+print(backTrack(anode))
